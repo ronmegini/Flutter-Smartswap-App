f48d6ce47c59fd1d4cba4657d8faf1d2a128af7b
ron34353435 <ron34353435@gmail.com>
2022-12-29 18:50:38 +0200

    nicer detail screen

--- lib/screens/contracts.dart
+++ lib/screens/contracts.dart
@@ -12,9 +12,24 @@ class ContractsTable extends StatelessWidget {
   ContractsTable({super.key});
 
   List<Map<String, dynamic>> contracts = [
-    {'name': 'a', 'desc': 'a'},
-    {'name': 'b', 'desc': 'b'},
-    {'name': 'c', 'desc': 'c'}
+    {
+      'name': 'תשלום למנקה',
+      'desc': 'תשלום חודשי למנקה',
+      'sideA': 'ניקיון פעמיים בחודש',
+      'sideB': 'תשלום קבוע של 500₪'
+    },
+    {
+      'name': 'עריכת וידיאו',
+      'desc': 'תשלום חד פעמי תמורת עריכת וידיאו',
+      'sideA': 'עריכת סרטון באורך 10 דקות',
+      'sideB': 'תשלום של 400₪'
+    },
+    {
+      'name': 'ועד בית',
+      'desc': 'תשלום ועד בית',
+      'sideA': 'אחזקת הבניין',
+      'sideB': 'תשלום חוזר של 150₪'
+    }
   ];
 
   @override
@@ -41,6 +56,7 @@ class ContractsTable extends StatelessWidget {
   }
 }
 
+/*
 class DetailsScreen extends StatelessWidget {
   DetailsScreen({super.key});
   @override
@@ -54,5 +70,79 @@ class DetailsScreen extends StatelessWidget {
         child: Text(argument['desc']),
       ),
     );
+  }
+}
+*/
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Map argument = ModalRoute.of(context)!.settings.arguments as Map;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Details'),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(children: [
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    argument['desc'],
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Column(
+                  children: [
+                    const Text(
+                      'Side A Agreement:',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      argument['sideA'],
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: Column(
+                  children: [
+                    const Text(
+                      'Side B Agreement:',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      argument['sideB'],
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ])));
   }
 }
